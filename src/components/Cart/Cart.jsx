af6e15622689079590c7af7fd94817bab29294fa
mariafrisancho <maria.frisancho02@gmail.com>
2022-06-07 22:29:18 -0500

    Crear Orden Firestore

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -24,17 +24,17 @@ const ComprarProductos = () => {
 
 const Cart = () => {
   const { carList, vaciarCarrito, precioTotal, EliminarItem, } = useCartContext()
-  const [dataForm, setdataForm] = useState([])
+  const [dataForm, setdataForm] = useState({ nombre: '', telefono: '', email: '' })
 
 
 
- async function generarOrden() {
+  async function generarOrden() {
+
     let orden = {}
 
-    orden.buyer=dataForm
-  
- 
-    // orden.buyer = { name: 'Moises', email: 'maria.frisancho@hotmail.com', phone: '989517823' }
+    orden.buyer = dataForm
+
+
     orden.total = precioTotal()
     orden.items = carList.map(cartItem => {
       const id = cartItem.id
@@ -54,55 +54,66 @@ const Cart = () => {
       .catch(error => console.log(error))
       .finally(() => vaciarCarrito())
 
-      //Actualizar stock
-      const queryCollecctionStock=collection(db,'productos')
-      const queryActualizarStock= query(
-        queryCollecctionStock,
-        where( documentId(),'in',carList.map(it=>it.id))
-      )
-      const batch=writeBatch(db)
-      await getDocs(queryActualizarStock)
-      .then(resp=>resp.docs.forEach(res=> batch.update(res.ref,{
-         stock: res.data().stock -carList.find(item=>item.id==res.id).cantidad
+    //Actualizar stock
+    const queryCollecctionStock = collection(db, 'productos')
+    const queryActualizarStock = query(
+      queryCollecctionStock,
+      where(documentId(), 'in', carList.map(it => it.id))
+    )
+    const batch = writeBatch(db)
+    await getDocs(queryActualizarStock)
+      .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
+        stock: res.data().stock - carList.find(item => item.id == res.id).cantidad
       })))
-      .finally(()=>console.log('Actualizado'))
-      batch.commit()
+      .finally(() => console.log('Actualizado'))
+    batch.commit()
 
 
   }
 
-  const handlerChange=(e)=>{
-    setdataForm([
+
+  const handlerChange = (e) => {
+    setdataForm({
       ...dataForm,
-      e.target.value
-     ]
+      [e.target.name]: e.target.value
+    }
     )
   }
   return (
     <>
-      {
-        precioTotal() !== 0 && <h2> Pedido</h2>
-      }
+
 
       <div>
-        {carList.map(product => <div key={product.id} >
-          <span> {product.nombre} </span>
-          <span className="badge bg-warning text-dark"> precio: {product.precio}</span>
-          <span className="badge bg-primary"> Cantidad: {product.cantidad}</span>
-          <span className="badge bg-dark"> Subtotal: {product.subtotal} </span>
-          <button className="btn btn-info btn-delete" onClick={() => EliminarItem(product.id)}>X</button>
+        <br />
+        <center>
 
-        </div>)}
 
-        <button onClick={generarOrden}>OrdenCompra</button>
+          {carList.map(product => <div key={product.id} >
+            <span> {product.nombre} </span>
+            <span className="badge bg-warning text-dark"> precio: {product.precio}</span>
+            <span className="badge bg-primary"> Cantidad: {product.cantidad}</span>
+            <span className="badge bg-dark"> Subtotal: {product.subtotal} </span>
+            <button className="btn btn-info btn-delete" onClick={() => EliminarItem(product.id)}>X</button>
+
+          </div>)}
+
+          {
+
+            precioTotal() !== 0 && <h2 className="badge bg-dark">El Precio Total es= {precioTotal()} </h2>
+
+
+          }
+
+    
+
+        </center>
 
       </div>
       <div>
-        {
-          precioTotal() !== 0 && <h2 className="badge bg-dark">El Precio Total es= {precioTotal()} </h2>
-        }
+
       </div>
-    <div>
+      <div>
+        <center>
         {
           precioTotal() !== 0 ?
 
@@ -114,50 +125,57 @@ const Cart = () => {
 
         }
 
-      </div> 
+        </center>
+     
+
+      </div>
       <div>
         {
-          carList.length !==0 &&
-         <center>
-          <form onSubmit={generarOrden} >
-            <h5>Orden de Compra</h5>
-            <input
-            className="form-comtrol"
-            type='text'
-            name='nombre'
-            placeholder="Ingrese Nombre"
-            value={dataForm.nombre}
-            onChange={handlerChange}
-            
-            /><br/>
-            <input
-            className="form-comtrol"
-            type='text'
-            name='telefono'
-            placeholder="Ingrese Telefono"
-            value={dataForm.telefono}
-            onChange={handlerChange}
-            
-            /><br/>
-            <input
-            className="form-comtrol"
-            type='email'
-            name='email'
-            placeholder="Ingrese Email"
-            value={dataForm.email}
-            onChange={handlerChange}
-            
-            /><br/>
-            <button className="btn btn-outline-primary" onClick={generarOrden}>Terminar Compra</button>
-           
-            
-          </form>
+          carList.length !== 0 &&
+          <center>
 
-         </center>
+<br/>
+
+            <h5>Datos del comprador</h5>
+            <input
+              className="form-comtrol"
+              type='text'
+              name='nombre'
+              placeholder="Ingrese Nombre"
+              value={dataForm.nombre}
+              onChange={handlerChange}
+
+            /><br />
+            <input
+              className="form-comtrol"
+              type='text'
+              name='telefono'
+              placeholder="Ingrese Telefono"
+              value={dataForm.telefono}
+              onChange={handlerChange}
+
+            /><br />
+            <input
+              className="form-comtrol"
+              type='email'
+              name='email'
+              placeholder="Ingrese Email"
+              value={dataForm.email}
+              onChange={handlerChange}
+
+            /><br /> <br />
+
+            <button onClick={generarOrden}>Terminar Compra</button>
+
+
+
+
+          </center>
 
         }
 
       </div>
+      
     </>
   )
 }
